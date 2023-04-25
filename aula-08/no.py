571e9693d9a084f43424494dbf89df3924c6daae
Kelvin <kelvinmarquesk@gmail.com>
2023-04-25 11:51:31 -0300

    Implementação dos métodos da pilha sequencial

---
+++ aula-08/no.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+
+class No:
+    def __init__(self, valor) -> None:
+        self.__valor = valor
+        self.__prox = None
+
+    # implementar proterty e setter
--- aula-08/pilha.py
+++ aula-08/pilha.py
@@ -1,23 +1,42 @@
 # -*- coding:utf-8 -*-
 
 class Pilha:
+    """
+    Pilha sequencial usando a estrutura lista do Python
+    """
     def __init__(self) -> None:
         self.__pilha = []
 
     def is_empty(self):
         return True if len(self.__pilha) == 0 else False
 
-    def push(self):
-        ...
+    def push(self, valor):
+        self.__pilha.append(valor)
 
     def pop(self):
-        ...
+        if self.is_empty():
+            raise Exception("Sorry, pilha vazia!!!")
+        else:
+            valor = self.__pilha.pop()
+            return valor
 
     def peek(self):
-        ...
+        if self.is_empty():
+            raise Exception("Sorry, pilha vazia!!!")
+        else:
+            valor = self.__pilha[-1]
+            return valor
 
     def list_items(self):
-        ...
+        if self.is_empty():
+            raise Exception("Sorry, pilha vazia!!!")
+        else:
+            print("Relação de itens na Pilha: \n")
+            # fazer sem o método reverse
+            pilha_invertida = self.__pilha.reverse() 
+            for item in self.__pilha:
+                print(item)
+            print("Base da Pilha")
 
     def get_size(self):
-        ...
+        return len(self.__pilha)
