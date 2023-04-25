38d1b437ad87bc5161e01486d15743c3203bb71c
Kelvin <kelvinmarquesk@gmail.com>
2023-04-25 11:59:28 -0300

    Adicionado o arquivo teste.py para casos de teste

---
+++ aula-08/teste.py
@@ -0,0 +1,21 @@
+from pilha import Pilha
+
+# Casos de teste
+pilha = Pilha()
+
+print(f"Tamanho da pilha: {pilha.get_size()}")
+print(f"Pilha vazia? {pilha.is_empty()}")
+
+pilha.push("Valor 1")
+pilha.push("Valor 2")
+pilha.push("Valor 3")
+pilha.push("Valor 4")
+pilha.push("Valor 5")
+
+print(f"Topo da pilha: {pilha.peek()}")
+print(f"Tamanho da pilha: {pilha.get_size()}")
+print(f"Removendo item da pilha: {pilha.pop()}")
+print(f"Tamanho da pilha: {pilha.get_size()}")
+
+pilha.list_items()
+
